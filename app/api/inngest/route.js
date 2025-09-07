267c2778bd520870812bbda9aa0f490b7b7e4beb
sudhanshu1711 <sudhanshu2679@gmail.com>
2025-09-07 15:09:05 +0530

    done with inngest setup

---
+++ app/api/inngest/route.js
@@ -0,0 +1,9 @@
+import { serve } from "inngest/next";
+
+import { inngest } from "@/lib/inngest/client";
+import { helloWorld } from "@/lib/inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [helloWorld],
+});
